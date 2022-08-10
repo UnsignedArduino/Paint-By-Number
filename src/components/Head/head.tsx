b1fa418c416754ef81c54c870fafe741a6354021
UnsignedArduino <UnsignedArduino@outlook.com>
2022-08-10 17:08:00 -0400

    Clear index page and add basic head component

---
+++ src/components/Head/head.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import type { HeadFC } from "gatsby";
+
+export const HeadComponentFactory = (pageTitle?: string): HeadFC => {
+  return (
+    () => {
+      return (
+        <>
+          {
+            pageTitle === "" || pageTitle == undefined ? 
+            <title>Paint by Number</title> : <title>{pageTitle} | Paint by Number</title>
+          }
+        </>
+      );
+    }
+  );
+}
+
+export default HeadComponentFactory;
