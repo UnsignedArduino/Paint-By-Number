26783da63486f9f8346115c58c53e79713283a9c
UnsignedArduino <UnsignedArduino@outlook.com>
2022-08-10 22:31:26 -0400

    Add navbar button for small screens

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -22,31 +22,36 @@ const NavbarComponent = ({ selected }: NavbarComponentParameterType): React.Reac
         <div className="container-fluid-sm">
           <a className="navbar-brand" href="/">Paint by Number</a>
         </div>
-        <ul className="navbar-nav">
-          {
-            Object.keys(NavbarLink).filter(
-              (_: string, index: number) => {
-                return index >= Object.keys(NavbarLink).length / 2;
-              }
-            ).map(
-              (value: string, index: number) => {
-                if (selected === index) {
-                  return (
-                    <li className="nav-item">
-                      <Link className="nav-link active" to={navbarLinks[index]}>{value}</Link>
-                    </li>
-                  );
-                } else {
-                  return (
-                    <li className="nav-item">
-                      <Link className="nav-link" to={navbarLinks[index]}>{value}</Link>
-                    </li>
-                  );
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul className="navbar-nav">
+            {
+              Object.keys(NavbarLink).filter(
+                (_: string, index: number) => {
+                  return index >= Object.keys(NavbarLink).length / 2;
                 }
-              }
-            )
-          }
-        </ul>
+              ).map(
+                (value: string, index: number) => {
+                  if (selected === index) {
+                    return (
+                      <li className="nav-item">
+                        <Link className="nav-link active" to={navbarLinks[index]}>{value}</Link>
+                      </li>
+                    );
+                  } else {
+                    return (
+                      <li className="nav-item">
+                        <Link className="nav-link" to={navbarLinks[index]}>{value}</Link>
+                      </li>
+                    );
+                  }
+                }
+              )
+            }
+          </ul>
+        </div>
       </div>
     </nav>
   );
